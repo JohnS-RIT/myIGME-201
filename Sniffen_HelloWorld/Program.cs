f67488edf611ad7c06cf4486ab7fd5af0a2ef2d4
John S <jts4605@rit.edu>
2020-08-20 21:01:18 -0400

    Testing commit to push of HelloWorld PE1

---
+++ Sniffen_HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sniffen_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("John Sniffen");
+            int a = 5;
+            int b = 3;
+            Console.WriteLine(a + b);
+        }
+    }
+}
