a15daf5eda001360223264da9319f561f4186a07
John S <jts4605@rit.edu>
2020-09-06 22:27:11 -0400

    Commit of PE8 Question 8

---
+++ PE8_Question8/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Question8
+{
+    //Class: program
+    //Purpose: console application that accepts a string and replaces all occurrences of the string "no" with "yes"
+    //Restrictions: none
+    class Program
+    {
+        //Method: main
+        //Purpose: console application that accepts a string and replaces all occurrences of the string "no" with "yes"
+        //Restrictions: none
+        static void Main(string[] args)
+        {
+            string userInput;
+
+            Console.Write("Please enter a word or sentence: ");
+            userInput = Console.ReadLine();
+
+            if (userInput == "no")
+            {
+                userInput = "yes";
+            }
+
+            Console.WriteLine("\n" + userInput + "\n");
+        }
+    }
+}
