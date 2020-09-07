c7cf57a014276523482cfae8c994b420b49d10cc
John S <jts4605@rit.edu>
2020-09-06 22:38:08 -0400

    Commit of PE8, Question 9

---
+++ PE8_Question9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Question9
+{
+    //Class: program
+    //Purpose: Add quotations to each word in a string
+    //Restrictions: none
+    class Program
+    {
+        //Method: main
+        //Purpose: Add quotations to each word in user inputted string by splitting to array
+        //Restrictions: none
+        static void Main(string[] args)
+        {
+            //Declare variable
+            string userInput;
+
+            //Take in sentence
+            Console.Write("Please enter a sentence: ");
+            userInput = Console.ReadLine();
+
+            //Convert to string array based on " "
+            string[] stringList = userInput.Split(' ');
+
+            //Foreach loop to take each element and print each with " before and after it
+            foreach (var s in stringList)
+            {
+                Console.Write("\"" + s + "\" ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
