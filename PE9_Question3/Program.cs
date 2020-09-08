f7f8127aeaaa4fb27d6bae3cc559d9b0ddd9c64b
John S <jts4605@rit.edu>
2020-09-08 16:49:39 -0400

    Commit of PE9, Question 3

---
+++ PE9_Question3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9_Question3
+{
+    //Class: Program
+    //Purpose: create delegate function for Console.ReadLine()
+    class Program
+    {
+        //Method: main
+        //Purpose: call a readLine delegate and output the return
+        static void Main(string[] args)
+        {
+            string returnedInput = ReadLine();
+            Console.WriteLine("\n" + returnedInput);
+        }
+
+        //Method: ReadLine
+        //Purpose: delegate for Console.ReadLine();
+        static string ReadLine()
+        {
+            string input = Console.ReadLine();
+            return input;
+        }
+    }
+}
