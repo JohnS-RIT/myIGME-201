de9aad18a9d47fba664c0e42373cfb09e9afed09
John S <jts4605@rit.edu>
2020-09-15 17:42:10 -0400

    Commit of Unit1Test, Question 3

---
+++ Unit1Test_Question3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit1Test_Question3
+{
+    //Class: Program
+    //Purpose: Create a delegate to impersonate Console.ReadLine()
+    //Restrictions: None
+    class Program
+    {
+        delegate string ReadLine();
+
+        //Method: Main
+        //Purpose: call a ReadLine delegate and output the return
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+            ReadLine readLine = new ReadLine(Console.ReadLine);
+            Console.Write("Input a string: ");
+            string userInput = readLine();
+            Console.WriteLine("You wrote: " + userInput);
+        }
+    }
+}
