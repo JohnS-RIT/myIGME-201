7f687bc0b979ae8f337a383e6d37282cfc78bdb0
John S <jts4605@rit.edu>
2020-09-15 22:36:09 -0400

    Commit of Unit1Test, Question 13 with comments

---
+++ Unit1Test_Question13/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit1Test_Question13
+{
+    //Class: Program
+    //Purpose: Ask user for name and give raise depenedent on name
+    //Restrictions: None
+    class Program
+    {
+        struct employee
+        {
+            public string sName;
+            public double dSalary;
+        }
+
+        //Method: Main
+        //Purpose: Ask user for name
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+            employee employee1 = new employee();
+
+            Console.Write("What's your name?: ");
+            employee1.sName = Console.ReadLine();
+            employee1.dSalary = 30000;
+
+            bool raise = GiveRaise(ref employee1);
+
+            if (raise)
+            {
+                Console.WriteLine("Congrats John, you got a raise!");
+                Console.WriteLine("New salary: " + employee1.dSalary);
+            }
+        }
+
+        //Takes in ref for employee struct and increases salary and returns true if employee's name = "John"
+        static bool GiveRaise(ref employee employee1)
+        {
+            if (employee1.sName == "John")
+            {
+                employee1.dSalary += 19999.99;
+                return true;
+            }
+            return false;
+        }
+    }
+}
