9b7b6da62523164142f185e7934ed5ccff6d7f9c
John S <jts4605@rit.edu>
2020-09-19 18:16:53 -0400

    Fixed library I hope, I had deleted the class Class1 on accident.

--- Vehicles/Class1.cs
+++ Vehicles/Class1.cs
@@ -6,54 +6,57 @@ using System.Threading.Tasks;
 
 namespace Vehicles
 {
-    public abstract class Vehicle
+    class Class1
     {
-        public virtual void LoadPassenger()
+        public abstract class Vehicle
+        {
+            public virtual void LoadPassenger()
+            {
+
+            }
+        }
+        public abstract class Car : Vehicle
         {
 
         }
-    }
-    public abstract class Car: Vehicle
-    {
+        public abstract class Train : Vehicle
+        {
+            public override void LoadPassenger()
+            {
 
-    }
-    public abstract class Train: Vehicle
-    {
-        public override void LoadPassenger()
+            }
+        }
+        public interface IPassengerCarrier
         {
 
         }
-    }
-    public interface IPassengerCarrier
-    {
+        public interface IHeavyLoadCarrier
+        {
 
-    }
-    public interface IHeavyLoadCarrier
-    {
+        }
+        public class Compact : Car, IPassengerCarrier
+        {
 
-    }
-    public class Compact: Car, IPassengerCarrier
-    {
+        }
+        public class SUV : Car, IPassengerCarrier
+        {
 
-    }
-    public class SUV: Car, IPassengerCarrier
-    {
+        }
+        public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier
+        {
 
-    }
-    public class Pickup: Car, IPassengerCarrier, IHeavyLoadCarrier
-    {
+        }
+        public class PassengerTrain : Train, IPassengerCarrier
+        {
 
-    }
-    public class PassengerTrain: Train, IPassengerCarrier
-    {
+        }
+        public class FreightTrain : Train, IHeavyLoadCarrier
+        {
 
-    }
-    public class FreightTrain: Train, IHeavyLoadCarrier
-    {
+        }
+        public class _424DoubleBogey : Train, IHeavyLoadCarrier
+        {
 
-    }
-    public class _424DoubleBogey: Train, IHeavyLoadCarrier
-    {
-
+        }
     }
 }
