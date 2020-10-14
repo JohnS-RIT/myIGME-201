519c610c7c2be89d7b0541f25b506d2371de4bc5
John S <jts4605@rit.edu>
2020-10-14 12:07:44 -0400

    Commit of completed code for Unit Test #2, Question 14

---
+++ UnitTest2_Question14/Friend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest2_Question14
+{
+    //Class: Friend
+    public class Friend
+    {
+        public string name;
+        public string greeting;
+        public DateTime birthdate;
+        public string address;
+    }
+}
---
+++ UnitTest2_Question14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest2_Question14
+{
+    //Class: Program
+    //Purpose: Create a 2 Friend objects to display info
+    //Restrictions: None
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Friend friend = new Friend();
+            Friend enemy = new Friend();
+
+            // create my friend Charlie Sheen
+            friend.name = "Charlie Sheen";
+            friend.greeting = "Dear Charlie";
+            friend.birthdate = DateTime.Parse("1967-12-25");
+            friend.address = "123 Any Street, NY NY 12202";
+
+            enemy.greeting = "Sorry Charlie";
+            enemy.address = "Return to sender.  Address unknown.";
+
+            Console.WriteLine($"friend.greeting => enemy.greeting: {friend.greeting} => {enemy.greeting}");
+            Console.WriteLine($"friend.address => enemy.address: {friend.address} => {enemy.address}");
+        }
+    }
+}
