1db39fe222fe14f7cf33dc4c1a7c6f1fc80a846d
John S <jts4605@rit.edu>
2020-11-07 18:52:00 -0500

    Commit of adjusted Dyscord form from PE 27

--- Dyscord/Form2.cs
+++ Dyscord/Form2.cs
@@ -53,6 +53,8 @@ namespace Dyscord
             this.sendButton.Click += new EventHandler(SendButton__Click);
             this.exitButton.Click += new EventHandler(ExitButton__Click);
             this.webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(WebBroswer1__DocumentCompleted);
+
+            this.FormClosing += new FormClosingEventHandler(Form__FormClosing);
         }
 
         private void LoginButton__Click(object sender, EventArgs e)
@@ -132,6 +134,12 @@ namespace Dyscord
             thread.Abort();
 
             Application.Exit();
+        }
+
+        private void Form__FormClosing(object sender, FormClosingEventArgs e)
+        {
+            listener.Close();
+            thread.Abort();
         }
 
         public void UpdateConversation(string text)
